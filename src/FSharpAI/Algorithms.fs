72f187d973d1ea29680b289f87e7cb24609a59f6
Riccardo <riccardo@teknology360.com>
2013-11-03 19:07:56 -0500

    First Commit

---
+++ src/FSharpAI/Algorithms.fs
@@ -0,0 +1,19 @@
+﻿module Algorithms
+
+open System
+open System.IO
+
+module Test = 
+    
+    let rec quickSort (data:int list) = 
+        match data with
+        | [] -> []
+        | [a] -> [a]
+        | head::tail ->
+            let l0 = 
+                tail
+                |> List.filter ((<=) head)
+            let l1 = 
+                tail
+                |> List.filter ((>) head)
+            quickSort(l0) @ [head] @ quickSort(l1)
---
+++ src/FSharpAI/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open Algorithms
+
+[<EntryPoint>]
+let main argv = 
+
+    let unsortedList = [-2; -3; 4; -1; 0; 1; 5; 7; -2]
+
+    let sortedList = Algorithms.Test.quickSort(unsortedList)
+    printfn "%A" sortedList
+
+    Console.ReadKey(true) |> ignore
+
+    0
